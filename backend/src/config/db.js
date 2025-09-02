127d5149e6c79cb0569ed496caedd1f0de0275d2
Akhil <akhil.dhawan@successive.tech>
2025-09-02 13:39:20 +0530

    expense-tracker

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-  try {
-    mongoose.connect(MONGO_URI)
-      .then(() => console.log("MongoDB connected"))
-      .catch((err) => console.error("MongoDB connection failed:", err.message));
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
-  }
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection failed:", err.message));
 };
